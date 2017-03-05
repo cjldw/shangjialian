c7d1f218d88001d2a17a7692124bbf4be8d15809
luowen <bigpao.luo@gmail.com>
2017-03-05 22:16:50 +0800

    display login user information

--- resources/views/backend/layouts/main.blade.php
+++ resources/views/backend/layouts/main.blade.php
@@ -32,8 +32,8 @@
                         <img src="/images/img.jpg" alt="..." class="img-circle profile_img">
                     </div>
                     <div class="profile_info">
-                        <span>Welcome,</span>
-                        <h2>John Doe</h2>
+                        <span>欢迎,</span>
+                        <h2>{{Auth::user() -> name}}</h2>
                     </div>
                 </div>
                 <!-- /menu profile quick info -->
