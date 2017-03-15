1808e20e099014b485e457673d41f2c5c890fdd7
luowen <bigpao.luo@gmail.com>
2017-03-15 23:12:20 +0800

    modified:   documents/database.sql

--- documents/database.sql
+++ documents/database.sql
@@ -190,3 +190,75 @@ create table if not exists bizman_mobile_skeleton (
     key index_updated_at(updated_at),
     key index_deleted_at(deleted_at)
 ) engine innodb charset utf8;
+
+-- 商家用户复制木板后, 新建自己活动表
+create table if not exists bizman_merchant_acts (
+    id int unsigned auto_increment,
+    merchant_id int unsigned not null default 0 comment '关联商家id',
+    openid char(28) not null default '' comment '关联用户opend_id',
+    title varchar(64) not null default '' comment '活动名称',
+    industry_id tinyint unsigned not null default 0 comment '行业id',
+    description varchar(100) not null default '' comment '活动简介',
+    cover_img varchar(256) null default null comment '封面图片',
+    banner_img varchar(256) null default null comment '活动内页图片',
+    color_plate varchar(512) null default null comment '活动背景等调色板',
+    background_music varchar(256) null default null comment '背景音乐',
+
+    act_start_time datetime null default null comment '活动开始时间',
+    act_end_time datetime null default null comment '活动结束时间',
+    act_prize_name varchar(32) null default null comment '奖品名称',
+    act_prize_cnt int unsigned not null default 0 comment '活动奖品数量',
+    prize_decorate varchar(16) null default null comment '奖品前部分修饰',
+    act_prize_unit varchar(4) null default null comment '奖品单位',
+    act_prize_desc varchar(512) null default null comment '奖品描述',
+    act_rule_decorate varchar(16) null default null comment '活动前缀修饰',
+    act_rule_cnt int unsigned not null default 0 comment '活动奖品数量',
+    act_rule_keywords varchar(16) null default '收集' comment '参与活动动作, [元宝, 星星啥的]',
+
+    act_images varchar(1024) null default null comment '图片序列化地址, 最多5张',
+
+    organizer_name varchar(32)  null default null comment '主办方姓名',
+    organizer_address varchar(32) null default null comment '主办方地址',
+    organizer_phone varchar(16) null default null comment '主办方电话',
+
+    about_us varchar(512) null default null comment '关于我们',
+    video_url varchar(256) null default null comment '视频地址',
+    link_name varchar(64) null default null comment '连接名称',
+    link_url varchar(256) null default null comment '连接地址',
+    act_type tinyint unsigned not null default 1 comment '[1/普通活动, 2/保留]',
+
+    is_recommend tinyint unsigned not null default 0 comment '活动推荐, [0/普通, 1/推荐]',
+    is_offshelf tinyint unsigned not null default 0 comment '活动是否下架,[0/正常, 1/下架]',
+    bizman_copy_cnt int unsigned not null default 0 comment '商家使用次数',
+    netizen_copy_cnt int unsigned not null default 0 comment '网名转发分享次数',
+
+    created_at datetime null default current_timestamp,
+    updated_at datetime null default null,
+    deleted_at datetime null default null,
+
+    primary key (id),
+    key index_title (title),
+    key index_act_start_time (act_start_time),
+    key index_act_end_time (act_end_time),
+    key index_act_type (act_type),
+    key index_is_recommend (is_recommend),
+    key index_created_at(created_at),
+    key index_updated_at(updated_at),
+    key index_deleted_at(deleted_at)
+
+
+) engine innodb charset utf8;
+
+-- 是否支持表
+create table if not exists bizman_supported (
+    id int unsigned auto_increment,
+    act_id int unsigned not null default 0 comment '活动id',
+    owner_openid char(28) not null default '' comment '平台opend_id',
+    support_openid char(28) not null default '' comment 'opend_id检测',
+
+    primary key index_supported (act_id, owner_openid, support_openid),
+    created_at datetime null default current_timestamp,
+    updated_at datetime null default null,
+    deleted_at datetime null default null
+
+) engine innodb charset utf8;
