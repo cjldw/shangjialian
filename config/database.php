d02ad5c7f6a2128ef42eca12fb18639710c31102
luowen <bigpao.luo@gmail.com>
2017-04-06 17:29:04 +0800

    change database password

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'shangjialian'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'Ihiyou@111111'),
+            'password' => env('DB_PASSWORD', '123456'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => 'bizman_',
