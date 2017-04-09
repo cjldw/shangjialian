947dd932374ae93842c66b580bdb3085b597b882
luowen <bigpao.luo@gmail.com>
2017-04-09 11:11:13 +0800

    user expired issue

--- app/Http/Middleware/UserDuration.php
+++ app/Http/Middleware/UserDuration.php
@@ -24,9 +24,15 @@ class UserDuration
             return $next($request);
         }
 
-        if(is_array($userInfo) && !empty($userInfo)
-            && isset($userInfo['isAvailable']) && $userInfo['isAvailable']) {
-            return $next($request);
+        if(is_array($userInfo) && !empty($userInfo)) {
+           if(isset($userInfo['isAvailable']) && $userInfo['isAvailable']) { // not login
+                return $next($request);
+            }
+
+            if(isset($userInfo['expired_at'])) { // user login
+               $expiredAt = (new \DateTime($userInfo['expired_at'])) -> getTimestamp();
+               if($expiredAt > time()) return $next($request);
+            }
         }
         throw new UserExpiredException("非法请求: [用户到期了]");
     }
