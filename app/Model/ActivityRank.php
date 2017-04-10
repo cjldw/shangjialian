bc18ee79e471c82b4546ecdba577c04d49d7f8f9
luowen <bigpao.luo@gmail.com>
2017-04-11 00:05:00 +0800

    create activity

--- app/Model/ActivityRank.php
+++ app/Model/ActivityRank.php
@@ -15,4 +15,21 @@ class ActivityRank extends Model
 {
     protected $table = "activity_rank";
 
+    protected $fillable = [
+        'act_id',
+        'merchant_id',
+        'openid',
+        'name',
+        'phone',
+        'spend_time',
+        'join_cnt',
+        'completed_cnt',
+        'level',
+        'helpers',
+        'is_completed',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
 }
