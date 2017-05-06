eb8b4b3f7a9f8dabe2d0eee9045a36781d3c1e36
luowen <bigpao.luo@gmail.com>
2017-05-06 13:45:45 +0800

    debug

--- app/Utils/WxJsUtils.php
+++ app/Utils/WxJsUtils.php
@@ -61,6 +61,7 @@ class WxJsUtils
                 'checkJsApi',
                 'onMenuShareTimeline',
                 'onMenuShareAppMessage',
+                /*
                 'onMenuShareQQ',
                 'onMenuShareWeibo',
                 'onMenuShareQZone',
@@ -94,6 +95,7 @@ class WxJsUtils
                 'addCard',
                 'chooseCard',
                 'openCard'
+                */
             ]
         ];
     }
